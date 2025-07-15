56af7607780394d63371e0154eed0d60303fe008
latteeea <kimtaeran4767@gmail.com>
2025-07-16 00:30:45 +0900

    add: mockProductData 추가

--- app/src/main/java/com/app/stronglife/data/model/Product.kt
+++ app/src/main/java/com/app/stronglife/data/model/Product.kt
@@ -1,2 +1,11 @@
 package com.app.stronglife.data.model
 
+data class Product(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val time: String,
+    val nutrition: String,
+    val imageUrl1: String,
+    val imageUrl2: String,
+)
--- app/src/main/java/com/app/stronglife/mock/MockProductData.kt
+++ app/src/main/java/com/app/stronglife/mock/MockProductData.kt
@@ -1,11 +1,36 @@
 package com.app.stronglife.mock
 
-data class Product(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val time: String,
-    val nutrition: String,
-    val imageUrl1: String,
-    val imageUrl2: String,
+import com.app.stronglife.data.model.Product
+
+val sampleProducts = listOf(
+    Product(
+        id = 1,
+        title = "칼로바이 부스터 복숭아라임맛",
+        description = "천연 카페인 300mg으로\n고강도 운동까지 거뜬하게",
+        time = "운동 전",
+        nutrition = "카페인 300mg의 고함량  L-아르기닌, EAA\n~카페인 300mg의 고함량 L-아르기닌, EAA",
+        imageUrl1 = "https://your-server.com/images/calobye.png",
+        imageUrl2 = "https://your-server.com/images/calobye.png"
+    ),
+    Product(
+        id = 2,
+        title = "삼대오백 프리워크아웃 포도맛",
+        description = "9가지 필수 아미노산 EAA와\n고함량 6,000mg 아르기닌의 강력한 조합",
+        time = "운동 중",
+        nutrition = "카페인 300mg의 고함량  L-아르기닌, EAA\n" +
+                "~카페인 300mg의 고함량 L-아르기닌, EAA",
+        imageUrl1 = "https://your-server.com/images/samdaepre.png",
+        imageUrl2 = "https://your-server.com/images/calobye.png"
+    ),
+    Product(
+        id = 3,
+        title = "얼티밋포텐션 EAA 사과맛",
+        description = "필수 아미노산 9종 복합제\n근합성부터 회복까지",
+        time = "운동 후",
+        nutrition = "카페인 300mg의 고함량  L-아르기닌, EAA\n" +
+                "~카페인 300mg의 고함량 L-아르기닌, EAA",
+        imageUrl1 = "https://your-server.com/images/samdaepre.png",
+        imageUrl2 = "https://your-server.com/images/calobye.png"
+    ),
+
 )
