28b3da9c30b14db3321a2ed019f20f5d041e8769
latteeea <kimtaeran4767@gmail.com>
2025-08-22 10:19:16 +0900

    add: ko, en 버전 strings resource 파일 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.compose.ui:ui-text-google-fonts:1.8.1")
     implementation("androidx.navigation:navigation-compose:2.7.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
