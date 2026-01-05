dfdb9ad7845aaca36515a0a0f2ae6bccd44ee044
latteeea <kimtaeran4767@gmail.com>
2026-01-05 11:23:59 +0900

    design: QuestionBarBox(3번 설문) UI 디자인

--- app/src/main/java/com/app/stronglife/ui/screen/EndScreen/QuestionBarBox.kt
+++ app/src/main/java/com/app/stronglife/ui/screen/EndScreen/QuestionBarBox.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -25,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
@@ -51,29 +55,30 @@ fun QuestionBarBox(
     onAnswered: (Int) -> Unit
 ) {
     val density = LocalDensity.current
-    val boxWidth = with(density) {1498f.toDp()}
-    val boxHeight = with(density) {448f.toDp()}
-    val titleTextSp = with(density) {44f.toSp()}
-    val indexTextSp = with(density) {36f.toSp()}
-    val dotSize = with(density) {30f.toDp()}
-    val selectedDotSize = with(density) {32f.toDp()}
-    val heightSpaceDp = with(density) {32f.toDp()}
+    val boxWidth = with(density) { 1498f.toDp() }
+    val boxHeight = with(density) { 448f.toDp() }
+    val titleTextSp = with(density) { 44f.toSp() }
+    val indexTextSp = with(density) { 36f.toSp() }
+    val dotSize = with(density) { 30f.toDp() }
+    val selectedDotSize = with(density) { 34f.toDp() }
+    val heightSpaceDp = with(density) { 40.toDp() }
     val blurRadiusPx = with(density) { 7.dp.toPx() }
-    val roundDp = with(density) {20f.toDp()}
-    val descTextSp = with(density) {28f.toSp()}
+    val roundDp = with(density) { 20f.toDp() }
+    val descTextSp = with(density) { 26f.toSp() }
 
-    var selectedScore by remember { mutableStateOf<Int?>(null) }
+    var selectedScore by remember { mutableStateOf<Int?>(3) } // preview용 3 선택
 
     LaunchedEffect(questionIndex) {
         selectedScore = null
     }
 
-    Column (
+    Column(
         modifier = Modifier.fillMaxWidth(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Box(
-            modifier = Modifier.size(boxWidth, boxHeight)
+            modifier = Modifier
+                .size(boxWidth, boxHeight)
                 .drawBehind {
                     drawIntoCanvas { canvas ->
                         val paint = Paint().asFrameworkPaint().apply {
@@ -94,16 +99,17 @@ fun QuestionBarBox(
                         )
                     }
                 }
-                .background(color = Color.White, RoundedCornerShape(roundDp))
+                .background(Color.White, RoundedCornerShape(roundDp))
         ) {
-
             Column(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 55.dp, vertical = 60.dp),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
+
+                Row(modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start) {
                     Text(
                         text = question.title,
                         fontFamily = Stronglife,
@@ -117,9 +123,7 @@ fun QuestionBarBox(
                     Text(
                         text = buildAnnotatedString {
                             append("(")
-                            withStyle(
-                                style = SpanStyle(color = mainRed)
-                            ) {
+                            withStyle(SpanStyle(color = mainRed)) {
                                 append(question.index.toString())
                             }
                             append("/3)")
@@ -131,50 +135,84 @@ fun QuestionBarBox(
                     )
                 }
 
-                Spacer(modifier = Modifier.height(heightSpaceDp))
+                Spacer(modifier = Modifier.height(100.dp))
 
                 Box(
-                    modifier = Modifier
-                        .fillMaxWidth(0.8f),
+                    modifier = Modifier.fillMaxWidth(0.9f).height(240.dp),
                     contentAlignment = Alignment.Center
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(3.dp)
-                            .background(mainRed.copy(alpha = 0.5f))
-                    )
 
-                    Row (
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .drawBehind {
+                                val centerY = size.height / 2f - 75.dp.toPx()
+                                drawRect(
+                                    color = mainRed.copy(alpha = 0.5f),
+                                    topLeft = androidx.compose.ui.geometry.Offset(130f, centerY),
+                                    size = androidx.compose.ui.geometry.Size(
+                                        width = size.width * 0.8f,
+                                        height = 4.dp.toPx()
+                                    )
+                                )
+                            },
                         verticalAlignment = Alignment.CenterVertically
-                    ){
-                        (1..5).forEach {
-                            score ->
-                            Column (
-                                horizontalAlignment = Alignment.CenterHorizontally
-                            ){
-                                Image(
-                                    painter = painterResource(
-                                        id = if (selectedScore == score) R.drawable.dot_selected else R.drawable.dot_noselected,
-                                    ),
-                                    contentDescription = "$score 점",
-                                    modifier = Modifier
-                                        .size(if (selectedScore == score) dotSize else selectedDotSize)
-                                        .clickable {
-                                            selectedScore = score
-                                            onAnswered(score)
+                    ) {
+                        (1..5).forEach { score ->
+                            val isSelected = selectedScore == score
+
+                            Box(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .fillMaxHeight()
+                                    .clickable {
+                                        selectedScore = score
+                                        onAnswered(score)
+                                    }
+                                    .drawBehind{
+                                        if (isSelected) {
+                                            drawRoundRect(
+                                                color = mainRed.copy(alpha = 0.05f),
+                                                cornerRadius = CornerRadius(16.dp.toPx())
+                                            )
                                         }
-                                )
+                                    },
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Column(
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Image(
+                                        painter = painterResource(
+                                            if (isSelected)
+                                                R.drawable.dot_selected
+                                            else
+                                                R.drawable.dot_noselected
+                                        ),
+                                        contentDescription = "$score 점",
+                                        modifier = Modifier
+                                            .size(if (isSelected) selectedDotSize else dotSize)
+                                    )
 
-                                Spacer(modifier = Modifier.height(heightSpaceDp))
+                                    Spacer(modifier = Modifier.height(heightSpaceDp))
 
-                                Text(
-                                    text = "${score}점",
-                                    fontFamily = Stronglife,
-                                    fontWeight = FontWeight.Medium,
-                                )
+                                    Text(
+                                        text = "${score}점",
+                                        fontFamily = Stronglife,
+                                        fontSize = descTextSp,
+                                        fontWeight = FontWeight.Medium,
+                                        color = black
+                                    )
+
+                                    Spacer(modifier = Modifier.height(4.dp))
+
+                                    Text(
+                                        text = question.labels[score - 1],
+                                        fontFamily = Stronglife,
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = descTextSp,
+                                        color = black,
+                                    )
+                                }
                             }
                         }
                     }
@@ -182,8 +220,9 @@ fun QuestionBarBox(
             }
         }
     }
-
+    Spacer(modifier = Modifier.height(56.dp))
 }
+
 
 @Preview(device = "spec:width=1920px,height=1080px,dpi=82")
 @Composable
